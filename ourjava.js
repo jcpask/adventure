1063d9d16590ccdca6659847d6410f7345784dfb
Christopher Paskewich <c.paskewich@centre.edu>
2019-09-17 11:01:05 -0400

    added new stuff

--- ourjava.js
+++ ourjava.js
@@ -1,1 +1,20 @@
 
+var sayHi=function()
+{
+    var node=document.createElement("h2");
+    console.log("node",node);
+    node.innerText="Your name is Brunnhilde";
+            
+    document.querySelector("body")
+        .appendChild(node);
+    node.setAttribute("onclick", "name()");
+
+}
+var name=function()
+{
+    var node=document.createButton("Your challenge!");
+    console.log("node",node);
+    node.innerText="You must fight a dragon";
+ }
+
+
