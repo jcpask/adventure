7a58c3c236ede79922e5addacfdd2262afaefd65
MaryAshby <54848877+MaryAshby@users.noreply.github.com>
2019-09-18 20:48:09 -0400

    take3 on button2

--- ourjava.js
+++ ourjava.js
@@ -5,13 +5,14 @@ var sayHi=function()
     console.log("node",node);
     node.innerText="Your name is Brunnhilde";
             
-    document.querySelector("body")
+    
+    var node=document.createElement("button");
+    console.log("node",node);
+    node1.innerText="Your Challenge!";
+    
+     document.querySelector("body")
         .appendChild(node);
     node.setAttribute("onclick", "name()");
-    
-    var x=document.createElement("button");
-    console.log("node1",node1);
-    node1.innerText="Your Challenge!";
 
 }
 var name=function()
