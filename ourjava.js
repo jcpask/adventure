cd9b555ed75b232bcb95fd3f89cec8431ebc463e
MaryAshby <54848877+MaryAshby@users.noreply.github.com>
2019-09-18 22:21:26 -0400

    9

--- ourjava.js
+++ ourjava.js
@@ -10,15 +10,14 @@ var sayHi=function()
     node.setAttribute("onclick", "name()");
 }
 
-var saybye=function()
 {
     function changeImage() {
             var image = document.getElementById('myImage');
             if (image.src.match("https://user-images.githubusercontent.com/54848877/65207863-30632e80-da61-11e9-84f3-68559dd98590.jpg")) {
-                image.src = "https://user-images.githubusercontent.com/54848877/65207863-30632e80-da61-11e9-84f3-68559dd98590.jpg;
+                image.src = "https://raw.githubusercontent.com/jcpask/adventure/master/attacking-dragon.png";
             }
             else {
-                image.src = "https://raw.githubusercontent.com/jcpask/adventure/master/attacking-dragon.png";
+                image.src = "https://user-images.githubusercontent.com/54848877/65207863-30632e80-da61-11e9-84f3-68559dd98590.jpg";
             }
         }
 
