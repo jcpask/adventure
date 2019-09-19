1dd9c3dafdd7a7f1bc970240d165070dba753f2c
MaryAshby <54848877+MaryAshby@users.noreply.github.com>
2019-09-18 21:45:00 -0400

    maybe

--- ourjava.js
+++ ourjava.js
@@ -10,5 +10,15 @@ var sayHi=function()
     node.setAttribute("onclick", "name()");
 }
 
+var saybye=function()
+{
+    var node=document.createElement("a");
+  node.setAttribute("href","https://github.com/jcpask/adventure/blob/master/41208645_10155996761217569_829956108729712640_n.jpg");
+    
+                
+     document.querySelector("body")
+        .appendChild(node);
+    node.setAttribute("onclick", "name()");
+}
 
 
