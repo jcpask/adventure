e9a258b1356d96228a759cf3c7d7ae2cb7556fda
MaryAshby <54848877+MaryAshby@users.noreply.github.com>
2019-09-19 01:10:52 -0400

    Update ourjava.js

--- ourjava.js
+++ ourjava.js
@@ -4,6 +4,11 @@ var sayHi=function()
     var node=document.createElement("h2");
     console.log("node",node);
     node.innerText="Your name is Brunnhilde";
+    
+         document.querySelector("body")
+        .appendChild(node);
+    node.setAttribute("onclick", "name()");
+
                 
 }
 
