fa760d2132070e4c97f91edeb741646ea1aa1644
MaryAshby <54848877+MaryAshby@users.noreply.github.com>
2019-09-18 21:19:50 -0400

    Well this works at least

--- ourjava.js
+++ ourjava.js
@@ -4,16 +4,12 @@ var sayHi=function()
     var node=document.createElement("h2");
     console.log("node",node);
     node.innerText="Your name is Brunnhilde";
-    
-     
-var node = document.createElement("img");
-img.src = "https://github.com/jcpask/adventure/blob/master/41208645_10155996761217569_829956108729712640_n.jpg";
-
-var src = document.getElementById("body");
-src.appendChild(img);           
+                
      document.querySelector("body")
         .appendChild(node);
     node.setAttribute("onclick", "name()");
 
 }
+
+
 
