c97c54e93185eca2b720cc7aa79b7d17d856d8fb
MaryAshby <54848877+MaryAshby@users.noreply.github.com>
2019-09-18 21:29:29 -0400

    7

--- ourjava.js
+++ ourjava.js
@@ -8,8 +8,15 @@ var sayHi=function()
      document.querySelector("body")
         .appendChild(node);
     node.setAttribute("onclick", "name()");
-
 }
 
-
+var child=function()
+{
+    var child=document.createElement("a");
+    child.setAttribute("href","img.src = "https://github.com/jcpask/adventure/blob/master/41208645_10155996761217569_829956108729712640_n.jpg");
+            
+     document.querySelector("body")
+        .appendChild(Your name is Brunnhilde);
+    node.setAttribute("onclick", "name()");           
+}                   
 
